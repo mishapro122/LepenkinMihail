f9a788f9347f8cea7e633bfd7edab63bb10bc646
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-10-05 15:44:00 +0300

    Create Interface.java

---
+++ homework3/Interface.java
@@ -0,0 +1,14 @@
+public class Interface {
+    public interface metod<A> {
+        void add(A el);
+
+        void get(int index);
+
+        void remove(int index);
+
+        void print();
+
+        void len();
+
+    }
+}
