4aba2991ccc04ed433420f532216281b03fbe7bf
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-10-13 23:33:42 +0300

    Update Sort.java

--- homework4/Sort.java
+++ homework4/Sort.java
@@ -1,6 +1,11 @@
 import java.util.*;
 
 public class Sort {
+    /**
+    * choosingSort - функция выбора нужной сортировки, на вход принимает массив, который нужно отсортировать, и тип необходимой сортировки
+    * пока на выбор дается только два вида сортировок только пузырьком и вставками
+    * если пользователь хочет использовать другую сортировку, то @throw IllegalStateException
+    */
     public static List<Integer> choosingSort(List<Integer> arr, String type) {
         if (type.equals(" пузырек")) {
             return BubbleSort.sort(arr);
