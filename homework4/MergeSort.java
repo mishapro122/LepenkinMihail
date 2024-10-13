f8b8c35e419808cf97ce4cf24be3b63748ded929
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-10-13 23:30:09 +0300

    Update MergeSort.java

--- homework4/MergeSort.java
+++ homework4/MergeSort.java
@@ -3,8 +3,17 @@ import java.util.*;
 import static java.util.List.*;
 
 public class MergeSort  {
+    /**
+    * SIZE - максимально возможный размер массива
+    */
     private static final int SIZE = 5000;
-
+    /**
+    * sort - функция сортировки массива, которая выводит копию отсортированного заданного массива
+    * сначала идет проверка на то, является ли массив пустым, если является, то выводит @throw NullPointerException
+    * потом идет проверка на размер, если размер массива больше чем максимальный, то выводит @throw IllegalStateException
+    * далее создается sortArr - копия изначального массива, которую мы будем сортировать
+    * в конце реализована сортировка вставками(она является встроенной сортировкой, поэтому для простоты используем Collections.sort() и вывод отсортировонного массива
+    */
     public static List<Integer> sort(List<Integer> arr) {
         if (arr == null) {
             throw new NullPointerException("нельзя отсортировать пустой массив");
