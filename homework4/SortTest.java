c1fa667af433e7c61d05d016eaf2f729f56da4b5
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-10-13 23:41:41 +0300

    Update SortTest.java

--- homework4/SortTest.java
+++ homework4/SortTest.java
@@ -4,37 +4,49 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MergeSortTest {
-
+  /**
+  * тест, который проверяет самый обычный случай
+  */
   public void testSortWithNormalList() {
     List<Integer> arr = Arrays.asList(5, 3, 8, 1, 2, 7, 4, 6);
     List<Integer> sortArr = MergeSort.sort(arr);
     assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8), sortArr);
   }
-
+  /**
+  * тест, который проверяет случай, когда заданный массив - пустой
+  */
   public void testSortWithEmptyList() {
     List<Integer> arr = List.of();
     List<Integer> sortArr = MergeSort.sort(arr);
     assertEquals(List.of(), sortArr);
   }
-
+  /**
+  * тест, который проверяет случай, когда заданный массив - из одного элемента
+  */
   public void testSortWithSingleElementList() {
     List<Integer> arr = List.of(52);
     List<Integer> sortArr = MergeSort.sort(arr);
     assertEquals(List.of(52), sortArr);
   }
-
+  /**
+  * тест, который проверяет случай, когда заданный массив - уже отсортирован
+  */
   public void testSortWithAlreadySortedList() {
     List<Integer> arr = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
     List<Integer> sortArr = MergeSort.sort(arr);
     assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8), sortArr);
   }
-
+  /**
+  * тест, который проверяет случай, когда заданный массив - отсортирован наоборот
+  */
   public void testSortWithReverseSortedList() {
     List<Integer> arr = Arrays.asList(8, 7, 6, 5, 4, 3, 2, 1);
     List<Integer> sortArr = MergeSort.sort(arr);
     assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8), sortArr);
   }
-
+  /**
+  * тест, который проверяет случай, когда заданный массив - имеет максимально возможную длинну
+  */
   public void testSortWithMaxElements() {
     List<Integer> arr = new ArrayList<>();
     for (int i = 0; i < 4999; i++) {
