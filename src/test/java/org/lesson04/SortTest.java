f1c80c32948ef043e392009382e6a7c014b6b159
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-10-22 21:58:39 +0300

    Update SortTest.java

--- src/test/java/org/lesson04/SortTest.java
+++ src/test/java/org/lesson04/SortTest.java
@@ -9,70 +9,70 @@ public class SortTest {
   @Test
   public void testSortWithNormalListPuzur() {
     List<Integer> arr = Arrays.asList(5, 3, 8, 1, 2, 7, 4, 6);
-    List<Integer> sortArr = Sort.choosingSort(arr, "пузырек");
+    List<Integer> sortArr = Sort.choosingSort(arr, SortTypes.valueOf("BUBBLE"));
     assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8), sortArr);
   }
 
   @Test
   public void testSortWithEmptyListPuzur() {
     List<Integer> arr = List.of();
-    List<Integer> sortArr = Sort.choosingSort(arr, "пузырек");
+    List<Integer> sortArr = Sort.choosingSort(arr, SortTypes.valueOf("BUBBLE"));
     assertEquals(List.of(), sortArr);
   }
 
   @Test
   public void testSortWithSingleElementListPuzur() {
     List<Integer> arr = List.of(52);
-    List<Integer> sortArr = Sort.choosingSort(arr, "пузырек");
+    List<Integer> sortArr = Sort.choosingSort(arr, SortTypes.valueOf("BUBBLE"));
     assertEquals(List.of(52), sortArr);
   }
 
   @Test
   public void testSortWithAlreadySortedListPuzur() {
     List<Integer> arr = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
-    List<Integer> sortArr = Sort.choosingSort(arr, "пузырек");
+    List<Integer> sortArr = Sort.choosingSort(arr, SortTypes.valueOf("BUBBLE"));
     assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8), sortArr);
   }
 
   @Test
   public void testSortWithReverseSortedListPuzur() {
     List<Integer> arr = Arrays.asList(8, 7, 6, 5, 4, 3, 2, 1);
-    List<Integer> sortArr = Sort.choosingSort(arr, "пузырек");
+    List<Integer> sortArr = Sort.choosingSort(arr, SortTypes.valueOf("BUBBLE"));
     assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8), sortArr);
   }
 
   @Test
   public void testSortWithNormalListMerger() {
     List<Integer> arr = Arrays.asList(5, 3, 8, 1, 2, 7, 4, 6);
-    List<Integer> sortArr = Sort.choosingSort(arr, "вставки");
+    List<Integer> sortArr = Sort.choosingSort(arr, SortTypes.valueOf("BUBBLE"));
     assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8), sortArr);
   }
 
   @Test
   public void testSortWithEmptyListMerge() {
     List<Integer> arr = List.of();
-    List<Integer> sortArr = Sort.choosingSort(arr, "вставки");
+    List<Integer> sortArr = Sort.choosingSort(arr, SortTypes.valueOf("MERGE"));
     assertEquals(List.of(), sortArr);
   }
 
   @Test
   public void testSortWithSingleElementListMerge() {
     List<Integer> arr = List.of(52);
-    List<Integer> sortArr = Sort.choosingSort(arr, "вставки");
+    List<Integer> sortArr = Sort.choosingSort(arr, SortTypes.valueOf("MERGE"));
     assertEquals(List.of(52), sortArr);
   }
 
   @Test
   public void testSortWithAlreadySortedListMerge() {
     List<Integer> arr = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
-    List<Integer> sortArr = Sort.choosingSort(arr, "вставки");
+    List<Integer> sortArr = Sort.choosingSort(arr, SortTypes.valueOf("MERGE"));
     assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8), sortArr);
   }
 
   @Test
   public void testSortWithReverseSortedListMerge() {
     List<Integer> arr = Arrays.asList(8, 7, 6, 5, 4, 3, 2, 1);
-    List<Integer> sortArr = Sort.choosingSort(arr, "вставки");
+    List<Integer> sortArr = Sort.choosingSort(arr, SortTypes.valueOf("MERGE"));
     assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8), sortArr);
   }
 }
