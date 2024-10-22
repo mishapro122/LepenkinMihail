8a82169e0d990bcbdbf80aee31c44edc679b51e5
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-10-22 21:59:12 +0300

    Update Main.java

--- src/main/java/org/lesson04/Main.java
+++ src/main/java/org/lesson04/Main.java
@@ -7,13 +7,16 @@ public class Main {
     Scanner input = new Scanner(System.in);
     System.out.print("Введите длинну массива: ");
     int n = input.nextInt();
-    System.out.println("Введите элементы массива, после них введите нужный тип сортировки: пузырек или вставки, сделайте все это через пробел");
+    System.out.println("Введите элементы массива");
     List<Integer> arr = new ArrayList<>();
     for (int i = 0; i < n; i++) {
       arr.add(input.nextInt());
     }
-    String type = input.nextLine();
-    List<Integer> otvet = Sort.choosingSort(arr, type);
+    Scanner input1 = new Scanner(System.in);
+    System.out.println("Введите тип сортировки BUBBLE или MERGE");
+    String type = input1.nextLine();
+      SortTypes tipe = SortTypes.valueOf((type));
+      List<Integer> otvet = Sort.choosingSort(arr, tipe);
     for (int i = 0; i < n; i++) {
       System.out.print(otvet.get(i) + " ");
     }
