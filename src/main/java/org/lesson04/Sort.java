8ecd61d720b1d7e45aee5fd562903bd898ffbc52
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-10-22 14:19:51 +0300

    Create Sort.java

---
+++ src/main/java/org/lesson04/Sort.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Sort {
+    /**
+     * choosingSort - функция выбора нужной сортировки, на вход принимает массив, который нужно отсортировать, и тип необходимой сортировки
+     * пока на выбор дается только два вида сортировок только пузырьком и вставками
+     * если пользователь хочет использовать другую сортировку, то @throw IllegalStateException
+     */
+    public static List<Integer> choosingSort(List<Integer> arr, String type) {
+        if (type.equals(" пузырек")) {
+            return BubbleSort.sort(arr);
+        }
+        else if (type.equals(" вставки")) {
+            return MergeSort.sort(arr);
+        }
+        else {
+            throw new IllegalStateException("выбранного типа не существует");
+        }
+    }
+}
