06bf32bc560d188a374477617536935517ffa1f9
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-10-22 23:10:03 +0300

    Create Camel.java

---
+++ org/example/animals/Camel.java
@@ -0,0 +1,10 @@
+public class Camel {
+  public static class Camel extends Herbivores implements Terrestrial {
+    public Camel() {
+      animalTitle = "Верблюд";
+    }
+    public void walk() {
+      System.out.println(animalTitle + " ходит");
+    }
+  }
+}
