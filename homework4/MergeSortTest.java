f195e5e4129e6b252450abdace7925b07c435003
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-10-22 13:43:58 +0300

    Update MergeSortTest.java

--- homework4/MergeSortTest.java
+++ homework4/MergeSortTest.java
@@ -7,6 +7,7 @@ public class MergeSortTest {
     /**
     * тест, который проверяет самый обычный случай
     */
+    @Test
     public void testSortWithNormalList()  {
         List<Integer> arr = Arrays.asList(5, 3, 8, 1, 2, 7, 4, 6);
         List<Integer> sortArr = MergeSort.sort(arr);
@@ -15,6 +16,7 @@ public class MergeSortTest {
     /**
     * тест, который проверяет случай, когда заданный массив - пустой
     */
+    @Test
     public void testSortWithEmptyList() {
         List<Integer> arr = List.of();
         List<Integer> sortArr = MergeSort.sort(arr);
@@ -23,6 +25,7 @@ public class MergeSortTest {
     /**
     * тест, который проверяет случай, когда заданный массив - из одного элемента
     */
+    @Test
     public void testSortWithSingleElementList() {
         List<Integer> arr = List.of(52);
         List<Integer> sortArr = MergeSort.sort(arr);
@@ -31,6 +34,7 @@ public class MergeSortTest {
     /**
     * тест, который проверяет случай, когда заданный массив - уже отсортирован
     */
+    @Test
     public void testSortWithAlreadySortedList() {
         List<Integer> arr = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
         List<Integer> sortArr = MergeSort.sort(arr);
@@ -39,6 +43,7 @@ public class MergeSortTest {
     /**
     * тест, который проверяет случай, когда заданный массив - отсортирован наоборот
     */
+    @Test
     public void testSortWithReverseSortedList() {
         List<Integer> arr = Arrays.asList(8, 7, 6, 5, 4, 3, 2, 1);
         List<Integer> sortArr = MergeSort.sort(arr);
@@ -47,6 +52,7 @@ public class MergeSortTest {
     /**
     * тест, который проверяет случай, когда заданный массив - имеет максимально возможную длинну
     */
+    @Test
     public void testSortWithMaxElements() {
         List<Integer> arr = new ArrayList<>();
         for (int i = 0; i < 5000; i++) {
