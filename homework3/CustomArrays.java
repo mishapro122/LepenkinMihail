b9b1250c2725720239eceedaf2057b6ce0123c6a
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-10-23 01:02:36 +0300

    Update CustomArrays.java

--- homework3/CustomArrays.java
+++ homework3/CustomArrays.java
@@ -4,11 +4,18 @@ public static class CustomArrays<A> implements Interface.metod<A> {
 
     private Object[] arr;
     private int last;
-
+    /**
+    * функция присоваиваний входных значений 
+    */
     public CustomArrays(Object[] arr, int last) {
         this.arr = arr;
         this.last = last;
     }
+    /**
+    * функция добавления элемента
+    * сначала идет проверка на то, является ли элемент нулевым или нет, если нулевой, то @RuntimeException
+    * создается массив arr, который является копией массива arr, но на одну еденицу длиннее, как раз в этой еденице будет новый элемент
+    */
     @Override
     public void add(A el) {
         try {
@@ -28,12 +35,19 @@ public static class CustomArrays<A> implements Interface.metod<A> {
             System.out.println("Аргумент не должен быть null");
         }
     }
-
+    /**
+    * функция получения элемента по индексу
+    */
     @Override
-    public void get(int index) {
-        System.out.println(arr[index]);
+    public <A> get(int index) {
+        return arr[index];
     }
-
+    /**
+    * функция удаления элемента
+    * сначала идет проверка на то, является ли индекс элемента, который мы хотим удалить последним
+    * если является, то мы просто создаем массив arr, который является копией массива arr, но на 1 еденицу короче
+    * если нет, то мы просто сдвигаем элементы начиная с с index + 1 на один шаг назад, потом мы просто создаем массив arr, который является копией массива arr, но на 1 еденицу короче
+    */
     @Override
     public void remove(int index) {
         if (index + 1 != last) {
