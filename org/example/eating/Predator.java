7139847d76a3493f5eaecb8e7edc26f1d3964c8e
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-10-23 00:08:52 +0300

    Update Predator.java

--- org/example/eating/Predator.java
+++ org/example/eating/Predator.java
@@ -1,11 +1,5 @@
 package org.example.wayOfEating;
 
 public abstract class Predator {
-  public void typeAnimal() {
-    System.out.println("Predator");
-  };
-  public void printEat() {
-    System.out.println("Meat");
-  };
   public void eat(String food) {}
 }
