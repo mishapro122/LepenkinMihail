a04f9881700e37011d5c5e62f4ea848ed16a0d12
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-10-22 13:45:20 +0300

    Update SortTest.java

--- homework4/SortTest.java
+++ homework4/SortTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SortTest {
-   /**
+    /**
     * тест, который проверяет самый обычный случай
     * тип сортировки - пузырек
     */
+    @Test
     public void testSortWithNormalListPuzur()  {
         List<Integer> arr = Arrays.asList(5, 3, 8, 1, 2, 7, 4, 6);
         List<Integer> sortArr = Sort.choosingSort(arr, "пузырек");
@@ -17,6 +18,7 @@ public class SortTest {
     * тест, который проверяет случай, когда заданный массив - пустой
     * тип сортировки - пузырек
     */
+    @Test
     public void testSortWithEmptyListPuzur() {
         List<Integer> arr = List.of();
         List<Integer> sortArr = Sort.choosingSort(arr, "пузырек");
@@ -26,6 +28,7 @@ public class SortTest {
     * тест, который проверяет случай, когда заданный массив - из одного элемента
     * тип сортировки - пузырек
     */
+    @Test
     public void testSortWithSingleElementListPuzur() {
         List<Integer> arr = List.of(52);
         List<Integer> sortArr = Sort.choosingSort(arr, "пузырек");
@@ -35,6 +38,7 @@ public class SortTest {
     * тест, который проверяет случай, когда заданный массив - уже отсортирован
     * тип сортировки - пузырек
     */
+    @Test
     public void testSortWithAlreadySortedListPuzur() {
         List<Integer> arr = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
         List<Integer> sortArr = Sort.choosingSort(arr, "пузырек");
@@ -44,6 +48,7 @@ public class SortTest {
     * тест, который проверяет случай, когда заданный массив - отсортирован наоборот
     * тип сортировки - пузырек
     */
+    @Test
     public void testSortWithReverseSortedListPuzur() {
         List<Integer> arr = Arrays.asList(8, 7, 6, 5, 4, 3, 2, 1);
         List<Integer> sortArr = Sort.choosingSort(arr, "пузырек");
@@ -53,6 +58,7 @@ public class SortTest {
     * тест, который проверяет случай, когда заданный массив - имеет максимально возможную длинну
     * тип сортировки - пузырек
     */
+    @Test
     public void testSortWithMaxElementsPuzur() {
         List<Integer> arr = new ArrayList<>();
         for (int i = 0; i < 5000; i++) {
@@ -64,6 +70,7 @@ public class SortTest {
     * тест, который проверяет самый обычный случай
     * тип сортировки - вставки
     */
+    @Test
     public void testSortWithNormalListMerger()  {
         List<Integer> arr = Arrays.asList(5, 3, 8, 1, 2, 7, 4, 6);
         List<Integer> sortArr = Sort.choosingSort(arr, "встваки");
@@ -73,6 +80,7 @@ public class SortTest {
     * тест, который проверяет случай, когда заданный массив - пустой
     * тип сортировки - вставки
     */
+    @Test
     public void testSortWithEmptyListMerge() {
         List<Integer> arr = List.of();
         List<Integer> sortArr = Sort.choosingSort(arr, "встваки");
@@ -82,6 +90,7 @@ public class SortTest {
     * тест, который проверяет случай, когда заданный массив - из одного элемента
     * тип сортировки - вставки
     */
+    @Test
     public void testSortWithSingleElementListMerge() {
         List<Integer> arr = List.of(52);
         List<Integer> sortArr = Sort.choosingSort(arr, "встваки");
@@ -91,6 +100,7 @@ public class SortTest {
     * тест, который проверяет случай, когда заданный массив - уже отсортирован
     * тип сортировки - вставки
     */
+    @Test
     public void testSortWithAlreadySortedListMerge() {
         List<Integer> arr = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
         List<Integer> sortArr = Sort.choosingSort(arr, "встваки");
@@ -100,6 +110,7 @@ public class SortTest {
     * тест, который проверяет случай, когда заданный массив - отсортирован наоборот
     * тип сортировки - вставки
     */
+    @Test
     public void testSortWithReverseSortedListMerge() {
         List<Integer> arr = Arrays.asList(8, 7, 6, 5, 4, 3, 2, 1);
         List<Integer> sortArr = Sort.choosingSort(arr, "встваки");
@@ -109,6 +120,7 @@ public class SortTest {
     * тест, который проверяет случай, когда заданный массив - имеет максимально возможную длинну
     * тип сортировки - вставки
     */
+    @Test
     public void testSortWithMaxElementsMerge() {
         List<Integer> arr = new ArrayList<>();
         for (int i = 0; i < 5000; i++) {
@@ -119,6 +131,7 @@ public class SortTest {
     /**
     * тест, который проверяет случай, когда заданный тип сортировки - не существует
     */
+    @Test
     public void testSortWithUnexistableType() {
         List<Integer> arr = new ArrayList<>();
         for (int i = 0; i < 4999; i++) {
