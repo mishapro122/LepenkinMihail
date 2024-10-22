915869a8c2767774251bdc2a3744f89f43bb2d58
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-10-22 14:19:23 +0300

    Create Main.java

---
+++ src/main/java/org/lesson04/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Введите длинну массива: ");
+    int n = input.nextInt();
+    System.out.println("Введите элементы массива, после них введите нужный тип сортировки: пузырек или вставки, сделайте все это через пробел");
+    List<Integer> arr = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      arr.add(input.nextInt());
+    }
+    String type = input.nextLine();
+    List<Integer> otvet = Sort.choosingSort(arr, type);
+    for (int i = 0; i < n; i++) {
+      System.out.print(otvet.get(i) + " ");
+    }
+  }
+}
