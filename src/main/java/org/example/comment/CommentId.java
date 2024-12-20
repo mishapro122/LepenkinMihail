8b413787b6f00d7b9b931d5634b688637c176e22
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-12-21 02:43:26 +0300

    Create CommentId.java

---
+++ src/main/java/org/example/comment/CommentId.java
@@ -0,0 +1,34 @@
+package org.example.comment;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Objects;
+
+public class CommentId {
+    @JsonValue
+    private final long value;
+
+    public CommentId(long value) {
+        this.value = value;
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this==o){
+            return true;
+        }
+        if (o==null || getClass()!=o.getClass()){
+            return false;
+        }
+        CommentId commentId= (CommentId) o;
+        return value==commentId.value;
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(value);
+    }
+}
