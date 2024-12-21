bdbcd654d3bbe26350875e3e324f0119f2cccf67
mihaillepenkin <93184271+mihaillepenkin@users.noreply.github.com>
2024-12-21 15:18:24 +0300

    Update ArticleService.java

--- src/main/java/org/example/service/ArticleService.java
+++ src/main/java/org/example/service/ArticleService.java
@@ -3,22 +3,24 @@ package org.example.service;
 import org.example.article.Article;
 import org.example.article.ArticleId;
 import org.example.articlerepository.ArticleRepository;
-import org.example.comment.Comment;
-import org.example.comment.CommentId;
 import org.example.commentrepository.CommentRepository;
 import org.example.exceptions.*;
+import org.example.topic.Topic;
+import org.example.topicrepository.TopicRepository;
+import org.example.transactionmaneger.TransactionManager;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class ArticleService {
     private final ArticleRepository articleRepository;
     private final CommentRepository commentRepository;
-
-    public ArticleService(ArticleRepository articleRepository, CommentRepository commentRepository) {
+    private final TopicRepository topicRepository;
+    private final TransactionManager transactionManager;
+    public ArticleService(ArticleRepository articleRepository, CommentRepository commentRepository, TopicRepository topicRepository, TransactionManager transactionManager) {
         this.articleRepository = articleRepository;
         this.commentRepository = commentRepository;
+        this.topicRepository = topicRepository;
+        this.transactionManager = transactionManager;
     }
     public List<Article> findAll(){
         return articleRepository.findAll();
@@ -31,20 +33,26 @@ public class ArticleService {
         }
     }
 
-    public Comment findCommentById(long id){
+    /*public Comment findCommentById(long id){
         try{
             return commentRepository.findById(id);
         }
         catch (CommentNotFoundException e){
             throw new CommentNotFoundException("cannot find comment with id="+id);
         }
-    }
+    }*/
 
     public long createArticle(String name, Set<String> tags){
         ArticleId articleId = articleRepository.generateId();
-        Article article=new Article(articleId, name, tags, new ArrayList<>());
+        Set<Topic> topics = new HashSet<>();
+        for (String tag : tags){
+            Topic topic = new Topic(articleId, topicRepository.generateId(), tag);
+            topics.add(topic);
+        }
+
+        Article article=new Article(articleId, name, topics, new ArrayList<>(), false);
         try{
-            articleRepository.create(article);
+            articleRepository.create(article, topicRepository);
             return article.getId();
         }
         catch (ArticleIdDuplicatedException e){
@@ -52,7 +60,36 @@ public class ArticleService {
         }
     }
 
-    public long createComment(long articleId, String text) {
+    public Set<Map<String,Long>> createArticles(List<Map<String,Set<String>>> articlesMap){
+        return transactionManager.inTransaction(() ->{
+            Set<Map<String, Long>> resultSet = new HashSet<>();
+            for (Map<String, Set<String>> map : articlesMap){
+                for (Map.Entry<String, Set<String>> entry : map.entrySet()){
+                    Article article = new Article(articleRepository.generateId(), entry.getKey(),Set.of(),new ArrayList<>(),false);
+                    try {
+                        articleRepository.create(article,topicRepository);
+                    }
+                    catch (ArticleIdDuplicatedException e){
+                        throw new ArticleCreateException("cannot create article");
+                    }
+                    resultSet.add(Map.of(article.getName(),article.getId()));
+                }
+                /*Article article = new Article(articleRepository.generateId(), entry.getKey(),Set.of(),new ArrayList<>(),false);
+                try {
+                    articleRepository.create(article,topicRepository);
+                }
+                catch (ArticleIdDuplicatedException e){
+                    throw new ArticleCreateException("cannot create article");
+                }
+                resultSet.add(Map.of(article.getName(),article.getId()));*/
+            }
+            return resultSet;
+        });
+    }
+
+
+
+    /*public long createComment(long articleId, String text) {
         CommentId commentId = commentRepository.generateId();
         Comment comment=new Comment(commentId, new ArticleId(articleId), text);
         try {
@@ -61,7 +98,7 @@ public class ArticleService {
         }catch (CommentIdDuplicatedException | ArticleNotFoundException e){
             throw new CommentCreateException("cannot create comment");
         }
-    }
+    }*/
 
     public void articleUpdate(long articleId, String name, Set<String> tags){
         Article article;
@@ -71,9 +108,10 @@ public class ArticleService {
         catch (ArticleNotFoundException e){
             throw new ArticleUpdateException("cannot find article with id="+articleId);
         }
-        articleRepository.update(article.withName(name).withTags(tags));
+        Set<Topic> newTopics = topicRepository.updateTags(tags, article.getTags(),new ArticleId(article.getId()));
+        articleRepository.update(article.withName(name).withTags(newTopics));
     }
-    public void commentUpdate(long commentId, String text){
+    /*public void commentUpdate(long commentId, String text){
         Comment newComment;
         try{
             newComment =commentRepository.findById(commentId);
@@ -82,7 +120,7 @@ public class ArticleService {
             throw new CommentUpdateException("cannot find comment with id="+commentId);
         }
         commentRepository.update(newComment.withText(text));
-    }
+    }*/
 
     public void deleteArticle(long articleId){
         Article article;
@@ -92,10 +130,10 @@ public class ArticleService {
         catch (ArticleNotFoundException e){
             throw new ArticleDeleteException("cannot delete book with id="+articleId);
         }
-        articleRepository.delete(articleId,commentRepository);
+        articleRepository.delete(articleId,commentRepository,topicRepository);
     }
 
-    public void deleteComment(long commentId){
+    /*public void deleteComment(long commentId){
         Comment comment;
         try {
             comment=commentRepository.findById(commentId);
@@ -103,5 +141,5 @@ public class ArticleService {
             throw new CommentDeleteException("cannot delete comment with id="+commentId);
         }
         commentRepository.delete(commentId);
-    }
+    }*/
 }
